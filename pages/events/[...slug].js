5f59d39974d8993c32c3028a67af8c72c24865e6
Hao Chen <hche8093@uni.sydney.edu.au>
2023-07-05 11:23:23 +0800

    Initial project

---
+++ pages/events/[...slug].js
@@ -0,0 +1,9 @@
+function FilteredEventsPage() {
+	return (
+		<div>
+			<h1>hi there</h1>
+		</div>
+	);
+}
+
+export default FilteredEventsPage;
---
+++ pages/events/[eventId].js
@@ -0,0 +1,9 @@
+function EventDetailPage() {
+	return (
+		<div>
+			<h1>hi there</h1>
+		</div>
+	);
+}
+
+export default EventDetailPage;
